447058055235591ae53c54f2cb7566e3d1ffc751
Nikita Mikhailov <n.mikhailov55@gmail.com>
2024-01-11 15:31:18 +0300

    Initial Commit

---
+++ Todo-Edu-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo-Edu-SwiftUI
+//
+//  Created by Никита Михайлов on 11.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo-Edu-SwiftUI/Todo_Edu_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_Edu_SwiftUIApp.swift
+//  Todo-Edu-SwiftUI
+//
+//  Created by Никита Михайлов on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_Edu_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
