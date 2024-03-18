78e78bb4cc0bc36b552a2f6b0e559ed601cef7b2
Nikita Mikhailov <n.mikhailov55@gmail.com>
2024-03-18 11:08:31 +0300

    ListViewModel

---
+++ Todo-Edu-SwiftUI/Models/TodoItem.swift
@@ -0,0 +1,8 @@
+//
+//  TodoItem.swift
+//  Todo-Edu-SwiftUI
+//
+//  Created by Никита Михайлов on 17.03.2024.
+//
+
+import Foundation
---
+++ Todo-Edu-SwiftUI/ViewModels/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  Todo-Edu-SwiftUI
+//
+//  Created by Никита Михайлов on 17.03.2024.
+//
+
+import Foundation
